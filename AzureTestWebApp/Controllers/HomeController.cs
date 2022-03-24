5354e6c23d512efe2cd9bc2c8410c2127a62a22c
jamieppp <jamiepatrickson@hotmail.co.uk>
2022-03-24 18:51:57 +0000

    app

---
+++ AzureTestWebApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureTestWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This application is an Azure test for the Web Apps";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Azure Web App";
+
+            return View();
+        }
+    }
+}
---
+++ AzureTestWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureTestWebApp.MvcApplication" Language="C#" %>
